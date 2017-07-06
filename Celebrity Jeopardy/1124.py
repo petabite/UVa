a506459f5d0545621e85e41eb3cd73c69dc578a5
Phil Z <Phil Z>
2017-07-06 10:55:21 -0700

    update celebrity jeopardy and tex quotes

--- Celebrity Jeopardy/1124.py
+++ Celebrity Jeopardy/1124.py
@@ -1,5 +1,9 @@
+#import stdin
 from sys import stdin
+
 def main():
+    #loop thru each line in the input file
+    #and print them.
     for line in stdin:
         print(line, end ='')
 if __name__ == '__main__':
--- TEX Quotes/272.py
+++ TEX Quotes/272.py
@@ -1,15 +1,37 @@
+#import stdin
 from sys import stdin
-# lines = [x.strip() for x in stdin.readlines()]
+
+#read the input file and store it as a list
 lines = stdin.readlines()
+
+#join the lines together into one string
 lines = ''.join(lines)
+
+# create an empty list that will later store
+# the positions of quotations marks
 quotes_indexes = []
+
+# loop thru each line in the input file and
+# look for quotation marks. If one is found,
+# add its index to the quotes_indexes list.
 for idx,char in enumerate(lines):
     if char == '"':
         quotes_indexes.append(idx)
+
+# turn the input file back into a list, with
+# one character as one element
 lines = list(lines)
+
+# loop thru this list and replace each quotation
+# using the indexes in quotes_indexes according to
+# the problem.
 for idx in quotes_indexes[::2]:
         lines[idx] = "``"
 for idx in quotes_indexes[1::2]:
         lines[idx] = "''"
+
+# join the input file again
 lines = ''.join(lines)
+
+# print the new, edited input file
 print(lines, end="")
